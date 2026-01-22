d7703881891291315d50abc8f8eb3126bfc5b2ac
ahmadmsvh <ahmadmsvh@gmail.com>
2026-01-21 22:07:41 -0500

    Update CI/CD pipeline and main.py

--- auth-service/main.py
+++ auth-service/main.py
@@ -2,12 +2,15 @@ import os
 from app.main import app
 from shared.config import get_settings
 from shared.logging_config import setup_logging
+from pathlib import Path
+from dotenv import load_dotenv
 
-
+_env_file = Path(__file__) / "shared" / "shared" / ".env"
+load_dotenv(dotenv_path=_env_file)
 
 settings = get_settings()
-setup_logging(service_name=settings.app.service_name, log_level=settings.app.log_level)
 
+setup_logging(service_name=os.getenv("SERVICE_NAME", "auth-service"), log_level=os.getenv("LOG_LEVEL", "DEBUG"))
 
 if __name__ == "__main__":
     import uvicorn
